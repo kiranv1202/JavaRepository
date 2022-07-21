bf96de4ba6017df95b49cf578597a5cbf9e66978
Kiran <kiranv1202@gmail.com>
2022-07-21 15:48:46 +0530

    Add files via upload

---
+++ PoS_Simulator/src/main/java/com/info/AppConfigs.java
@@ -0,0 +1,6 @@
+package com.info;
+
+public class AppConfigs {
+    public final static String applicationID = "PosSimulator";
+    public final static String bootstrapServers = "localhost:9091,localhost:9092,localhost:9093";
+}
---
+++ PoS_Simulator/src/main/java/com/info/PosSimulator.java
@@ -0,0 +1,55 @@
+package com.info;
+
+import com.info.types.PosInvoice;
+import com.info.serde.JsonSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class PosSimulator {
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+
+        String topicName = "pos";
+        int noOfProducers = 2;
+        int produceSpeed = 100;
+
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, AppConfigs.applicationID);
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        KafkaProducer<String, PosInvoice> kafkaProducer = new KafkaProducer<>(properties);
+        ExecutorService executor = Executors.newFixedThreadPool(noOfProducers);
+        final List<RunnableProducer> runnableProducers = new ArrayList<>();
+
+        for (int i = 0; i < noOfProducers; i++) {
+            RunnableProducer runnableProducer = new RunnableProducer(i, kafkaProducer, topicName, produceSpeed);
+            runnableProducers.add(runnableProducer);
+            executor.submit(runnableProducer);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            for (RunnableProducer p : runnableProducers) p.shutdown();
+            executor.shutdown();
+            logger.info("Closing Executor Service");
+            try {
+                executor.awaitTermination(produceSpeed * 2, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+
+    }
+}
